710884cd651a8ff7e5df19c4049185b5dccdc8af
Thiago Proença Idro <thiagoidro.11ti@gmail.com>
2025-12-04 18:39:37 -0300

    chore: adicionar esqueleto dos scripts cliente.js e funcionario.js

---
+++ src/main/webapp/js/cliente.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        // dados serão adicionados pelo membro responsável
+    },
+    methods: {
+        // loadMesas()
+        // abrirModal()
+        // enviarReserva()
+    }
+});
---
+++ src/main/webapp/js/funcionario.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        // dados serão adicionados pelo membro responsável
+    },
+    methods: {
+        // loadStatus()
+        // atualizarAgora()
+    },
+    // created() {
+    //     // setInterval para atualizar automaticamente
+    // }
+});
