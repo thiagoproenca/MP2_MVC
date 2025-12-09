d55ea752844915256895fdb8c4ef1da0c26b42ad
GustavoAndreas <90465034+GustavoAndreas@users.noreply.github.com>
2025-12-08 21:24:39 -0300

    Algumas alterações

--- src/main/java/com/restaurante/controller/StatusMesasController.java
+++ src/main/java/com/restaurante/controller/StatusMesasController.java
@@ -15,7 +15,18 @@ public class StatusMesasController extends HttpServlet {
         res.setCharacterEncoding("UTF-8");
 
         // JSON mockado
-        String json = "[{\"mesa\": 1, \"ocupada\": false}, {\"mesa\": 2, \"ocupada\": true}]";
+        String json = "[" +
+                "{\"mesa\": 1, \"lugares\": 4, \"ocupada\": false}," +
+                "{\"mesa\": 2, \"lugares\": 2, \"ocupada\": true}," +
+                "{\"mesa\": 3, \"lugares\": 6, \"ocupada\": false}," +
+                "{\"mesa\": 4, \"lugares\": 4, \"ocupada\": true}," +
+                "{\"mesa\": 5, \"lugares\": 8, \"ocupada\": false}," +
+                "{\"mesa\": 6, \"lugares\": 2, \"ocupada\": false}," +
+                "{\"mesa\": 7, \"lugares\": 4, \"ocupada\": true}," +
+                "{\"mesa\": 8, \"lugares\": 6, \"ocupada\": false}," +
+                "{\"mesa\": 9, \"lugares\": 2, \"ocupada\": true}," +
+                "{\"mesa\": 10, \"lugares\": 4, \"ocupada\": false}" +
+                "]";
 
         res.getWriter().write(json);
     }
